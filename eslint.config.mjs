5ca4abbd72ac3867a9416376474978a0a408c7e9
Aarav Malani <aarav.malani@gmail.com>
2026-05-26 23:57:24 +0800

    feat: add ESLint and Prettier + tsconfigs

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import eslintConfigPrettierFlat from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    ignores: ["dist", "coverage"],
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettierFlat,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["src/tsconfig.json", "src/tsconfig.test.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/no-unnecessary-type-assertion": "error",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/require-await": "error",
+      "@typescript-eslint/return-await": ["error", "in-try-catch"],
+    },
+  },
+  {
+    files: ["src/**/*.js"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+]);
