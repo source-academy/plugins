30341f91e321481ca2f0798089ec644039451055
Aarav Malani <aarav.malani@gmail.com>
2026-05-28 19:44:02 +0800

    fix: Gemini issues

--- lib/build.ts
+++ lib/build.ts
@@ -5,23 +5,36 @@ import { spawn } from "child_process";
 const program = new Command();
 
 export async function generateManifest() {
-    await fs.mkdir("dist");
+    await fs.mkdir("dist", { recursive: true });
     await Promise.all(packageJSON.workspaces.map(async (workspace) => {
         const manifest: Record<string, unknown> = {};
-        const pluginType = workspace.split("/").slice(-2)[0];
-        for await (const file of fs.glob(`${workspace}/manifest.json`)) {
-            const manifest = JSON.parse(await fs.readFile(file, "utf-8"));
-            const folderName = file.split("/").slice(0, -1).join("/");
-            manifest[folderName] = manifest;
+        const pluginType = workspace.split("/").slice(-3)[0];
+        for await (const file of fs.glob(`${workspace}manifest.json`)) {
+            const manifestFile = JSON.parse(await fs.readFile(file, "utf-8"));
+            const folderName = file.split("/").slice(-2)[0];
+            manifest[folderName] = manifestFile;
         }
         await fs.writeFile(`dist/${pluginType}.json`, JSON.stringify(manifest, null, 2));
     }));
 }
-
+export async function spawnPromise(...args: Parameters<typeof spawn>) {
+    return new Promise<void>((resolve, reject) => {
+        const child = spawn(...args);
+        child.on("close", (code) => {
+            if (code === 0) {
+                resolve();
+            }
+            else {
+                reject(new Error(`Process exited with code ${code}`));
+            }
+        }
+        );
+    });
+}
 export async function build(extraArgs: string[] = []) {
-    await Promise.allSettled([
+    await Promise.all([
         generateManifest(),
-        spawn("yarn", ["workspaces", "foreach", "-Apt", "run", "build", ...extraArgs], { stdio: "inherit" })
+        spawnPromise("yarn", ["workspaces", "foreach", "-Apt", "run", "build", ...extraArgs], { stdio: "inherit" })
     ])
 }
 
