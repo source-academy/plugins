69b867e4a57b6e130a686f207649095b2821aaaf
Aarav Malani <aarav.malani@gmail.com>
2026-06-16 11:37:28 +0530

    chore: migrate to vitest

--- src/runner/test/src/__tests__/runner.test.ts
+++ src/runner/test/src/__tests__/runner.test.ts
@@ -1,5 +1,7 @@
 import { TestPlugin } from "..";
 import { CHANNEL_ID } from "@sourceacademy/common-test";
+import { test, expect } from "vitest";
+
 test("should have a valid channel id", () => {
   expect(TestPlugin.channelAttach).toEqual([CHANNEL_ID]);
 });
