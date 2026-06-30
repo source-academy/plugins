a86fded1d30faee95e59e11f461f5f1fdcdf7e27
Aarav Malani <aarav.malani@gmail.com>
2026-06-30 16:54:10 +0800

    feat: add module loader packages

---
+++ src/common/module-loader/src/index.ts
@@ -0,0 +1,24 @@
+export const WEB_ID = "__web_module_loader";
+export const RUNNER_ID = "__runner_module_loader";
+
+export const CHANNEL_ID = "module_config";
+
+export enum ModuleLoaderMessageType {
+  REQUEST_MODULE = "request_module",
+  MODULE_RESPONSE = "module_response",
+  MODULE_ERROR = "module_error"
+}
+
+export type ModuleLoaderMessage = 
+  |{
+  type: ModuleLoaderMessageType.REQUEST_MODULE;
+  moduleName: string;
+} | {
+  type: ModuleLoaderMessageType.MODULE_RESPONSE;
+  moduleURL: string;
+  tabs: string[];
+} | {
+  type: ModuleLoaderMessageType.MODULE_ERROR;
+  error: string;
+}
+
---
+++ src/runner/module-loader/src/index.ts
@@ -0,0 +1,59 @@
+import { CHANNEL_ID, ModuleLoaderMessageType, RUNNER_ID, type ModuleLoaderMessage } from "@sourceacademy/common-module-loader";
+import {
+  checkIsPluginClass,
+  type IChannel,
+  type IConduit,
+  type IPlugin,
+} from "@sourceacademy/conductor/conduit";
+import type { IModulePlugin } from "@sourceacademy/conductor/module";
+import type { IRunnerPlugin } from "@sourceacademy/conductor/runner";
+
+export class ModuleLoaderRunnerPlugin implements IPlugin {
+  readonly id: string = RUNNER_ID;
+  static readonly channelAttach = [CHANNEL_ID];
+  private readonly __moduleRequestChannel: IChannel<ModuleLoaderMessage>;
+  private readonly __conductor: IRunnerPlugin;
+  static instance: ModuleLoaderRunnerPlugin | null = null;
+  constructor(
+    conduit: IConduit,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [moduleRequestChannel]: IChannel<any>[],
+    conductor: IRunnerPlugin
+  ) {
+    this.__moduleRequestChannel = moduleRequestChannel;
+    this.__conductor = conductor;
+    ModuleLoaderRunnerPlugin.instance = this;
+  }
+  async requestModule(moduleName: string): Promise<IModulePlugin> {
+    return new Promise((resolve, reject) => {
+      const handleResponse = async (msg: ModuleLoaderMessage) => {
+        this.__moduleRequestChannel.unsubscribe(handleResponse);
+        if (msg.type === ModuleLoaderMessageType.MODULE_RESPONSE) {
+          const plugin = await import(msg.moduleURL)
+            .then(module => {
+              return module.default;
+            })
+          const pluginObj = this.__conductor.registerPlugin(plugin, {
+            tabs: msg.tabs,
+            loadTab: (tabName: string) => {
+              if (!msg.tabs.includes(tabName)) {
+                throw new Error(`Tab ${tabName} not found in module ${moduleName}`);
+              }
+              this.__conductor.hostLoadPlugin(tabName);
+            }
+          }) as IModulePlugin;
+          await pluginObj?.initialise();
+          resolve(pluginObj);
+        } else if (msg.type === ModuleLoaderMessageType.MODULE_ERROR) {
+          reject(new Error(msg.error));
+        }
+      };
+      this.__moduleRequestChannel.subscribe(handleResponse);
+      this.__moduleRequestChannel.send({
+        type: ModuleLoaderMessageType.REQUEST_MODULE,
+        moduleName,
+      });
+    })
+  }
+}
+checkIsPluginClass(ModuleLoaderRunnerPlugin);
