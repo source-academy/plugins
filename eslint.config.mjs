e98dbcf60b19580375bd06d183b461008dfceb6d
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2026-08-06 21:52:03 +0800

    Update linting configuration to bring it inline with other repos

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,58 @@
-import eslintConfigPrettierFlat from "eslint-config-prettier/flat";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
+// @ts-check
+
+import eslintConfigPrettierFlat from 'eslint-config-prettier/flat';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import vitest from '@vitest/eslint-plugin';
 
 export default defineConfig([
   {
-    ignores: ["**/dist", "**/node_modules", "coverage"],
+    ignores: ['**/dist', '**/node_modules', 'coverage'],
   },
   tseslint.configs.recommended,
   eslintConfigPrettierFlat,
   {
-    files: ["src/**/*.{ts,tsx}"],
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
         projectService: {
-          allowDefaultProject: ['vitest.config.ts']
+          allowDefaultProject: ['vitest.config.ts'],
         },
         tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
+      '@typescript-eslint': tseslint.plugin,
     },
     rules: {
-      "@typescript-eslint/no-namespace": "off",
-      "@typescript-eslint/ban-ts-comment": "warn",
-      "@typescript-eslint/no-import-type-side-effects": "error",
-      "@typescript-eslint/no-unnecessary-type-assertion": "error",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/only-throw-error': 'error',
-      "@typescript-eslint/require-await": "error",
-      "@typescript-eslint/return-await": ["error", "in-try-catch"],
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/return-await': ['error', 'in-try-catch'],
     },
   },
   {
-    files: ["src/**/*.js"],
+    files: ['**/__tests__/**/*.test.{ts,tsx}'],
+    extends: [vitest.configs.recommended],
+    plugins: {
+      vitest,
+    },
     rules: {
-      "@typescript-eslint/no-require-imports": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      'vitest/prefer-import-in-mock': 'error',
+      'vitest/prefer-vi-mocked': 'error',
+    },
+  },
+  {
+    files: ['src/**/*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
 ]);
